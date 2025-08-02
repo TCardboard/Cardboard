7a627fbe9e465ce70f5f75de91dec66896e124b2
BreadButterCat <40633228+tulza@users.noreply.github.com>
2025-08-02 13:14:41 +1200

    feat: add convex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,35 +4,38 @@ import type { Metadata } from "next";
 import { Geist_Mono, Manrope } from "next/font/google";
 import { TanstackClientProvider } from "@/libs/tanstack";
 import { RoutingDevTools } from "@/utils/devtools/DevToolsProvider";
+import { ConvexClientProvider } from "@/providers/ConvexProvider";
 
 const manrope = Manrope({
-	variable: "--font-manrope",
-	subsets: ["latin"],
+  variable: "--font-manrope",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: "",
-	description: "",
+  title: "",
+  description: "",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body
-				className={`${manrope.className} ${geistMono.variable} antialiased`}
-			>
-				{process.env.NODE_ENV === "development" && <RoutingDevTools />}
-				<TanstackClientProvider>{children}</TanstackClientProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={`${manrope.className} ${geistMono.variable} antialiased`}
+      >
+        {process.env.NODE_ENV === "development" && <RoutingDevTools />}
+        <ConvexClientProvider>
+          <TanstackClientProvider>{children}</TanstackClientProvider>
+        </ConvexClientProvider>
+      </body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+"use client";
+
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
-	return (
-		<SidebarProvider>
-			<AppSidebar />
-			<main className="flex flex-1 flex-col p-4">
-				<div className="relative aspect-square size-[250px]"></div>
-			</main>
-		</SidebarProvider>
-	);
+  const tasks = useQuery(api.tasks.get);
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="flex flex-1 flex-col p-4">
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <div className="bg-white">
+            tasks: {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+          </div>
+        </main>
+      </main>
+    </SidebarProvider>
+  );
 }
