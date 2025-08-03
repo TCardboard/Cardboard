69ce5aa3969d4d37091802f03a1b1e940054e055
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2025-08-03 17:47:57 +1200

    capitalise urls for getting images

--- src/libs/cards/index.ts
+++ src/libs/cards/index.ts
@@ -1,6 +1,10 @@
 export const getCardUrl = (cardDetails: string) => {
   const split = cardDetails.split("-");
-  const rank = split[0];
-  const suit = split[1];
+  const rank = capitalize(split[0]);
+  const suit = capitalize(split[1]);
   return `/Cards/${suit}/${rank}.webp`;
 };
+
+function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
