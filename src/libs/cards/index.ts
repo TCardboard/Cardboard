a92a06ae53053dccf9e45668ae442cf0cbe3dd99
EzekielKo <118594978+EzekielKo@users.noreply.github.com>
2025-08-03 12:44:26 +1200

    change function to accept .webp

--- src/libs/cards/index.ts
+++ src/libs/cards/index.ts
@@ -2,5 +2,5 @@ export const getCardUrl = (cardDetails: string) => {
   const split = cardDetails.split("-");
   const rank = split[0];
   const suit = split[1];
-  return `/Cards/${suit}/${rank}.png`;
+  return `/Cards/${suit}/${rank}.webp`;
 };
