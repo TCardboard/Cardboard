ab3c129e237e53eb1e9501eda57db8fa602bc355
Oculux314 <nate.williamson.nz@gmail.com>
2025-08-03 14:25:51 +1200

    fix login with localstorage

--- src/libs/utils/hooks.ts
+++ src/libs/utils/hooks.ts
@@ -1,7 +1,7 @@
 "use client";
 
-import { useLocalStorage } from "@/hooks/useLocalStorage";
 import type { UserType } from "@/utils/types";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
 export const useLocalPlayer = () => {
   const [player, setPlayer] = useLocalStorage<UserType | null>("player", null);
